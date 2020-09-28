efcd0d959590d60230f7a8d3b74470356229d44e
Andrew Patrick <andrew.prsilva@gmail.com>
2020-09-28 19:56:05 -0300

    Comments added

--- src/main/java/com/kaffa/project/KaffaMainFrame.java
+++ src/main/java/com/kaffa/project/KaffaMainFrame.java
@@ -3,6 +3,20 @@ package com.kaffa.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+	/* Rest API returns a JSON
+	 * How it works:
+	 * localhost:8080/users - To return all clients - GET
+	 * localhost:8080/users/{id} - To return an specific user - GET
+	 * localhost:8080/users/Object JSON - To save an user in database - POST
+	 * localhost:8080/user/{id} - To delete an user - DELETE
+	 * localhost:8080/user/{id}/Object JSON - To update an user in database - PUT
+	 * 
+	 * Access H2 DATABASE:
+	 * localhost:8080/h2-console
+	 * username = kaffa
+	 * password =
+	 */
+
 @SpringBootApplication
 public class KaffaMainFrame {
 
